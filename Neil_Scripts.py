5a2339b5b6ba5e790267d3f7f2396bf5994674eb
neilcpart <98389321+neilcpart@users.noreply.github.com>
2022-01-25 10:32:36 +0000

    Add files via upload
    python test

---
+++ Neil_Scripts.py
@@ -0,0 +1,13 @@
+
+
+import numpy as np
+import pandas as pd
+import sys
+
+sys.___stdout__=sys.stdout
+
+# iterated over keys and values in a loop (page 77)
+
+df = pd.DataFrame(np.random.randn(4,3),columns=['A','B','C'])
+
+for key,value in df.iteritems():
